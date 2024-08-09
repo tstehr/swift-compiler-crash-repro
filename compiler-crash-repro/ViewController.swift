d37bc044f70c715cdc07290491ef8581cc11d4d8
Tilman Stehr <tilman@tilman.ninja>
2024-08-09 16:53:24 +0200

    Add SwiftRater 2.0.0 to dependencies via SPM
    This crashes the swift compiler in Xcode 15.4 and newer.

--- compiler-crash-repro/ViewController.swift
+++ compiler-crash-repro/ViewController.swift
@@ -6,12 +6,15 @@
 //
 
 import UIKit
+import SwiftRater
 
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    SwiftRater.check()
+
   }
 
 
