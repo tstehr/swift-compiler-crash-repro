c4a202f4bd215fec25ab12e42d6893cd2e27afad
Tilman Stehr <tilman@tilman.ninja>
2024-08-09 16:48:48 +0200

    Initial Commit

---
+++ compiler-crash-repro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  compiler-crash-repro
+//
+//  Created by Tilman Stehr on 09.08.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
